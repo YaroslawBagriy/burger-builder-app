54a45a02ee26352a76b2c80a621369de57d79e6b
yaroslawbagriy <yaroslawbagriy@gmail.com>
2020-06-17 08:56:51 -0500

    added logout option

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -12,9 +12,9 @@ const toolbar = ( props ) => (
             <Logo />
         </div>
         <nav className={classes.DesktopOnly}>
-            <NavigationItems />
+            <NavigationItems isAuthenticated={props.isAuth}/>
         </nav>
     </header>
 );
 
-export default toolbar;
+export default toolbar; 

774657aeb7763305f9985d35b966ec0c3c354148
yaroslawbagriy <yaroslawbagriy@gmail.com>
2020-06-23 22:33:05 -0500

    added auth keys

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCejYgXtAHnfaz892gqjAE_743A4SDA3N0';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCejYgXtAHnfaz892gqjAE_743A4SDA3N0';
         }
         axios.post(url, authData)
             .then(response => {

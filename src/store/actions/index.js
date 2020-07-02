dca2a40d349584398a34d6ca8901876765febf1b
yaroslawbagriy <yaroslawbagriy@gmail.com>
2020-07-02 18:17:08 -0500

    converted BurgerBuilder and Orders to use Redux Sagas

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,12 +1,20 @@
 export {
     addIngredient,
     removeIngredient,
-    initIngredients
+    initIngredients,
+    setIngredients,
+    fetchIngredientsFailed
 } from './burgerBuilder';
 export {
     purchaseBurger,
     purchaseInit,
-    fetchOrders
+    fetchOrders,
+    purchaseBurgerStart,
+    purchaseBurgerFail,
+    purchaseBurgerSuccess,
+    fetchOrdersSuccess,
+    fetchOrdersStart,
+    fetchOrdersFail
 } from './order';
 export {
     auth,
